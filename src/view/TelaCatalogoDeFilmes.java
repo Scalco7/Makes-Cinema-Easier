86c49ea9b59aa17d72e9abf7b1df9394f2141941
Felipe Scalco <fscalco7@gmail.com>
2025-02-06 00:08:41 -0300

    update 2

--- src/view/TelaCatalogoDeFilmes.java
+++ src/view/TelaCatalogoDeFilmes.java
@@ -6,13 +6,15 @@ import model.entities.Filme;
 public class TelaCatalogoDeFilmes extends javax.swing.JFrame {
 
     private static TelaCatalogoDeFilmes unicCatalogoDeFilmes;
-    
-    private ArrayList<Filme> filmes;
+
+    private ArrayList<Filme> filmes = new ArrayList<>();
 
     private TelaCatalogoDeFilmes() {
-        initComponents();
         filmes.add(new Filme(1, "Aranha verso", "Homem aranha ganha de td mundo", "12+", "Dublado", 120, ""));
         filmes.add(new Filme(2, "Eu vinho do futuro", "UMa comédia sobre vinhos mt loka", "Livre", "Legendado", 101, ""));
+
+        initComponents();
+        renderizaFilmes();
     }
 
     public static TelaCatalogoDeFilmes geraCatalogoDeFilmes() {
@@ -37,7 +39,8 @@ public class TelaCatalogoDeFilmes extends javax.swing.JFrame {
     private void initComponents() {
 
         BotaoEntrar = new javax.swing.JButton();
-        filme = new javax.swing.JPanel();
+        Catalogo = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -48,40 +51,51 @@ public class TelaCatalogoDeFilmes extends javax.swing.JFrame {
             }
         });
 
-        filme.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        Catalogo.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        javax.swing.GroupLayout filmeLayout = new javax.swing.GroupLayout(filme);
-        filme.setLayout(filmeLayout);
-        filmeLayout.setHorizontalGroup(
-            filmeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 158, Short.MAX_VALUE)
+        javax.swing.GroupLayout CatalogoLayout = new javax.swing.GroupLayout(Catalogo);
+        Catalogo.setLayout(CatalogoLayout);
+        CatalogoLayout.setHorizontalGroup(
+            CatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 589, Short.MAX_VALUE)
         );
-        filmeLayout.setVerticalGroup(
-            filmeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 196, Short.MAX_VALUE)
+        CatalogoLayout.setVerticalGroup(
+            CatalogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 338, Short.MAX_VALUE)
         );
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel1.setText("Catalogo");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(553, Short.MAX_VALUE)
-                .addComponent(BotaoEntrar)
-                .addContainerGap())
             .addGroup(layout.createSequentialGroup()
                 .addGap(19, 19, 19)
-                .addComponent(filme, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(BotaoEntrar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(Catalogo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 13, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(BotaoEntrar)
-                .addGap(27, 27, 27)
-                .addComponent(filme, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(148, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(BotaoEntrar)
+                        .addGap(27, 27, 27))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)))
+                .addComponent(Catalogo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -94,6 +108,13 @@ public class TelaCatalogoDeFilmes extends javax.swing.JFrame {
     private void goToLogin() {
         dispose();
         TelaLogin.geraLogin().abrirTela();
+    }
+    
+    private void renderizaFilmes(){
+        /*for(int i = 0; i < filmes.size(); i++){
+            JPanel filme = new javax.swing.JPanel().add(new javax.swing.JLabel());
+            Catalogo.add();
+        }*/
     }
 
     /**
@@ -140,6 +161,7 @@ public class TelaCatalogoDeFilmes extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BotaoEntrar;
-    private javax.swing.JPanel filme;
+    private javax.swing.JPanel Catalogo;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
